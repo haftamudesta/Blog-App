d0bea8ca77cb4ff1b81819627cd0bec9418f6613
haftamudesta <desta451616@hotmail.com>
2023-08-28 23:50:22 +0300

    Modify according to reviewers requirment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @posts = @user.posts.order(created_at: :asc)
+    
   end
 
   def show
-    @post = @user.post.find(params[:id])
+    
   end
 end
