c523506a8456f2616268d4b2860a26386c5696f7
haftamudesta <desta451616@hotmail.com>
2023-08-28 12:08:13 +0300

    Add controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,26 @@
 class PostsController < ApplicationController
-    def index
-        @posts=@user.posts.order(created_at: :asc)
-    end
-    def show
-        @post=@user.post.find(params[:id])
+  def index
+    @posts = @user.posts.order(created_at: :asc)
+  end
 
+  def show
+    @post = @user.post.find(params[:id])
+  end
 
+  def new
+    @post = Post.new
+  end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
 
-        
-    end
-    def new
-        @post=Post.new
-    end
-    def create
-        @post=Post.new(post_params)
-        if @post.save
-            redirect_to @post
-        else
-            render "new"
-        end
-    end
-    def post_params
-        params.require(:post).permit(:title,:text,:comments_counter,:likes_counter)
-    end
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
-    def index
-        @users=User.all
-    end
-    def show
-        @user=User.find(params[:id])
-    end
-    def user_params
-        params.require(:user).permit(:name,:photo, :bio,:posts_counter)
-    end
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
