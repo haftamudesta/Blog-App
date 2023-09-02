379b27128cab15bf56f9dc7ec657e948ee113e69
haftamudesta <desta451616@hotmail.com>
2023-09-02 07:28:58 +0300

    Add forms to blog app

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    puts 'Current User'
+
+    @post = Post.new(post_params)
+    @post.author = current_user
+    respond_to do |_format|
+      if @post.save
+        redirect_to user_posts_path(current_user)
+      else
+        render :new
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+end
