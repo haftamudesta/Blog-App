cb23fbcb8287ca0cef923d4a7ac671aadf8522cd
haftamudesta <desta451616@hotmail.com>
2023-09-06 12:19:08 +0300

    Include n+1 performance

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    @comments = @post.comments.includes(%i[author post])
+     @comments = @post.comments.includes(%i[author post])
   end
 
   def new
