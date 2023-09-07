f082187e066ab84f4f2faa317fabf0836f55105e
haftamudesta <desta451616@hotmail.com>
2023-09-07 14:33:56 +0300

    Add initial configuration

---
+++ spec/integration/user_index_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+RSpec.describe 'user, Index page', :focus, type: :intgration do
+  before :each do
+    @photo = 'https://unsplash.com/photos/F_-0BxGuVvo',
+             @user1 = User.new(
+               id: 4,
+               name: 'Tom', photo: @photo, bio: 'Teacher from Mexico.', posts_counter: 0
+             )
+    Post.new(
+      id: 1,
+      title: 'Hello one',
+      text: 'this is mu first post',
+      likes_counter: 0,
+      comments_counter: 0,
+      author: @user1
+    )
+  end
+  it 'should show the user profile picture' do
+    visit users_path(user_id: 4)
+    expect(page).to have_css("img[src*='#{@user1.photo}']")
+  end
+  it('can see user name off all other users') do
+    visit users_path
+    expect(page).to have_content("User Name: #{@user1.name}")
+  end
+
+  it('can see number of posts each user has written') do
+    visit users_path
+    expect(page).to have_content('Number of posts: 6')
+  end
+  it 'When I click on a user, I am redirected to that users show page' do
+    visit users_path
+    page.all(:link, 'name_container_link').last.click
+    expect(page).to have_current_path('/users/4')
+  end
+end
