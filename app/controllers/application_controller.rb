f11eb948f36c49435faf3da840e0881021b9fc71
haftamudesta <desta451616@hotmail.com>
2023-09-07 19:13:21 +0300

    Add some css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password, :posts_counter, :photo) }
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name,
+    devise_parameter_sanitizer.permit(:sign_up) { |s| s.permit(:name, :bio, :email, :password, :posts_counter, :photo) }
+    devise_parameter_sanitizer.permit(:account_update) do |s|
+      s.permit(:name,
                :bio,
                :email,
                :password,
