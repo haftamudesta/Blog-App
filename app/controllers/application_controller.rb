3e4f681d8c7a16c5304b891a16b6d5c60883e3d9
haftamudesta <desta451616@hotmail.com>
2023-09-08 10:14:16 +0300

    Modify Ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user = User.first
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |s| s.permit(:name, :bio, :email, :password, :posts_counter, :photo) }
+    devise_parameter_sanitizer.permit(:account_update) do |s|
+      s.permit(:name,
+               :bio,
+               :email,
+               :password,
+               :current_password,
+               :photo)
+    end
   end
 end
