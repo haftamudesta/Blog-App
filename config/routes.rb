e40f98f5de30ca3e5a100e24df52a02a832efc73
haftamudesta <desta451616@hotmail.com>
2023-09-09 15:54:39 +0300

    Add api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show, :new, :create, :destroy] do
+          resources :comments, only: [:index, :new, :create, :destroy]
+          resources :likes, only: [:create]
+        end
+      end
+    end
+  end
 end
